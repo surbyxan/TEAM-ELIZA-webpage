f6f93a0b1274af5f16e6010200e62756487a30e6
Malin Andréasson <malandre@kth.se>
2025-03-27 16:42:13 +0100

    more design updates

--- elizawebsite/src/App.jsx
+++ elizawebsite/src/App.jsx
@@ -6,22 +6,44 @@ function App() {
 
   return (
    <div>
-      <h1 class="headers">TEAM ELIZA</h1>
+      <div class="topbar">
+      <h1 class="headers">TEAM <span class="highlight">ELIZA</span></h1>
+      </div>
       <div class='body'>
         <div class='infobox'>
           <div >
             <h3 class="headers">About us</h3>
+            <p>Team Eliza is a project group that is producing a webapplication for synchronizing drones. We consist of 
+              2nd year ICT engineering students that study at KTH. The project started the 27th of March and will continue development in 
+              three sprints and be finished 16th of May.
+            </p>
           </div>
           <div>
             <h3 class="title">Drone<span class="highlight">Zone</span></h3>
+            <p>
+            DroneZone is the webapplication for synchronizing and viewing currently 
+            active and flying drones in Sweden. With the application you can easily fly you drone without
+            interuptning other airbourne objects.
+
+            </p>
           </div>
         </div>
         <div class='visualbox'>
           <div >
             <h3 class="headers">TEAM MEMBERS</h3>
+            <span>
+            <p>We are Team Eliza!</p>
+              
+            </span>
+
           </div>
           <div>
-            <p>visual representative of progress</p>
+            <h5>Lastets news from DroneZone</h5>
+            <p>
+              We have started the development on the application and are in
+              the beginning of our frist sprint out of three. The basic application structure are done 
+              and we are in the progress of designing and implementing the foundation of our ideas.
+            </p>
           </div>
         </div>
       </div>  
