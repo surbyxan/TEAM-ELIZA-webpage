d5467cc5f9d0045ec5b1742b358fb499041c78ca
Malin Andréasson <malandre@kth.se>
2025-03-28 16:02:51 +0100

    spellchecked the texts

--- elizawebsite/src/App.jsx
+++ elizawebsite/src/App.jsx
@@ -18,18 +18,18 @@ function App() {
         <div class='infobox'>
           <div >
             <h3 class="headers">About us</h3>
-            <p>Team Eliza is a project group that is producing a webapplication for synchronizing drones. We consist of 
-              2nd year ICT engineering students that study at KTH. The project started the 27th of March and will continue development in 
-              three sprints and be finished 16th of May.
+            <p>Team Eliza is a project group developing a web application for synchronizing drones. 
+              We consist of second-year ICT engineering students studying at KTH.
+              The project started on March 27th and will continue development over three sprints,
+              concluding on May 16th.
             </p>
           </div>
           <div class='appbox'>
             <h3 class="title">Drone<span class="highlight">Zone</span></h3>
             <p>
-            DroneZone is the webapplication for synchronizing and viewing currently 
-            active and flying drones in Sweden. With the application you can easily fly your drone without
-            interuptning other airbourne objects.
-
+            DroneZone is a web application for synchronizing and monitoring active, 
+            flying drones in Sweden. With this application, you can easily fly your drone
+            without interrupting other airborne objects.
             </p>
           </div>
         </div>
@@ -46,9 +46,9 @@ function App() {
               <h4>Lastets news from DroneZone</h4>
               <p class="date" >March 27th</p>
               <p>
-                We have started the development on the application and are in
-                the beginning of our frist sprint out of three. The basic application structure are done 
-                and we are in the progress of designing and implementing the foundation of our ideas.
+              We have started the development of the application and are at the beginning of our first 
+              sprint out of three. The basic application structure is done, and we are in the process of 
+              designing and implementing the foundation of our ideas.
               </p>
           </div>
         </div>
