02dbe4771f4ab1a1f68ff373c2daebea6483e856
Malin Andréasson <malandre@kth.se>
2025-04-10 10:45:22 +0200

    finalfinal fix to the footer and window sizes

--- elizawebsite/src/App.jsx
+++ elizawebsite/src/App.jsx
@@ -6,26 +6,26 @@ function App() {
 
   return (
    <div>
-      <div class="topbar">
-        <h1 class="headers">TEAM <span class="highlight">ELIZA</span></h1>
-        <span class='contactbox'>
+      <div className="topbar">
+        <h1 className="headers">TEAM <span className="highlight">ELIZA</span></h1>
+        <span className='contactbox'>
           <button>
-            <a href="mailto:teameliza@patron.me" class="btn2"><span class="spn2">Contact Us</span></a>
+            <a href="mailto:teameliza@patron.me" className="btn2"><span className="spn2">Contact Us</span></a>
           </button>
          </span>
       </div>
-      <div class='mainbody'>
-        <div class='infobox'>
+      <div className='mainbody'>
+        <div className='infobox'>
           <div >
-            <h3 class="headers">About us</h3>
+            <h3 className="headers">About us</h3>
             <p>Team Eliza is a project group developing a web application for synchronizing drones. 
               We consist of second-year ICT engineering students studying at KTH.
               The project started on March 27th and will continue development over three sprints,
               concluding on May 16th.
             </p>
           </div>
-          <div class='appbox'>
-            <h3 class="title">Drone<span class="highlight">Zone</span></h3>
+          <div className='appbox'>
+            <h3 className="title">Drone<span className="highlight">Zone</span></h3>
             <p>
             DroneZone is a web application for synchronizing and monitoring active, 
             flying drones in Sweden. With this application, you can easily fly your drone
@@ -34,18 +34,18 @@ function App() {
             </p>
           </div>
         </div>
-        <div class='visualbox'>
-          <div class='teambox'>
-            <h3 class="headers">TEAM MEMBERS</h3>
+        <div className='visualbox'>
+          <div className='teambox'>
+            <h3 className="headers">TEAM MEMBERS</h3>
             <span>
               <Teammember></Teammember>
-            <p class="teamelizatext">We are Team Eliza!</p>
+            <p className="teamelizatext">We are Team Eliza!</p>
             </span>
 
           </div>
-          <div class='newsbox'>
+          <div className='newsbox'>
               <h4>Lastets news from DroneZone</h4>
-              <p class="date" >April 10th</p>
+              <p className="date" >April 10th</p>
               <p>
               The second sprint has started. In this sprint, the focus is on the backend and database.
               In the last sprint, we accomplished our sprint goal by building the webpage, 
@@ -57,7 +57,7 @@ function App() {
         </div>
       </div>  
           <div className='contactinfo'>
-              <span class="mailtext">
+              <span className="mailtext">
                 More Questions? Reach us on mail! 
               <a href="mailto:teameliza@proton.me"> teameliza@proton.me </a>
               </span>
