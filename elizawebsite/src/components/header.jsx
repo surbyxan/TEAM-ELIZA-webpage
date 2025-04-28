a857d7ea8f7b84dab646c77572d24e0e3844c64f
Malin Andreasson <malandre@kth.se>
2025-04-28 14:03:12 +0200

    header fixes for smaller screens

--- elizawebsite/src/components/header.jsx
+++ elizawebsite/src/components/header.jsx
@@ -7,19 +7,19 @@ export function Header() {
 	
 	return(
 		<div className="topbar">
-        	<h1 className="text-5xl mt-9 mb-9">
+        	<h1 className="text-5xl mt-9 mb-8">
 				TEAM <span className="text-primary-yellow">ELIZA</span>
 			</h1>
-        	<div className="flex items-center gap-4">
-				<div className="flex items-center mr-10 h-12 bg-gray-200 rounded-2xl">
+        	<div className='navbar'>
+				<div className="flex items-center mr-10 h-12 bg-gray-200 rounded-2xl ">
 					<button
-					className="text-gray-700 bg-transparent ml-2 font-bold text-sm p-2 w-16 hover:scale-107 transition-all duration-200"
+					className="text-gray-700 bg-transparent ml-2 font-bold text-sm p-1 w-16 hover:scale-107 transition-all duration-200"
 					onClick={() => navigate("/")}
 					>
 						Home
 					</button>
 					<button
-					className="text-gray-700 bg-transparent ml-2 font-bold text-sm p-2  w-16 hover:scale-107 transition-all duration-200"
+					className="text-gray-700 bg-transparent mr-2 font-bold text-sm p-1 w-16 hover:scale-107 transition-all duration-200"
 					onClick={() => navigate("/expo")}
 					>
 						Expo
