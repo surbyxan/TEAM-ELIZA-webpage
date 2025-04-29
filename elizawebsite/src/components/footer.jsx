6b983dfd06e5dd1902e3ac2b7b91c3947fd359d6
Malin Andreasson <malandre@kth.se>
2025-04-29 10:25:43 +0200

    alignment issues

--- elizawebsite/src/components/footer.jsx
+++ elizawebsite/src/components/footer.jsx
@@ -4,7 +4,7 @@ import '../App.css'
 export function Footer() {
 	return (
 		<div className='footer'>
-			<div className="italic text-center h-10 gap-50">
+			<div className="italic text-left h-10 gap-50">
 		  		More Questions? Reach us on mail! 
 				<a href="mailto:teameliza@proton.me"> teameliza@proton.me </a>
 			</div>
